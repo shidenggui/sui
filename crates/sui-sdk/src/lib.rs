d31f1a5eb98b19c7db1c26ffb248ac212cb10e32
shidenggui <longlyshidenggui@gmail.com>
2024-05-21 14:40:03 +0800

    fix typo

--- crates/sui-sdk/src/lib.rs
+++ crates/sui-sdk/src/lib.rs
@@ -226,7 +226,7 @@ impl SuiClientBuilder {
 
         if let Some((key, value)) = self.extra_header {
             headers.insert(
-                key.as_str(),
+                key.leak(),
                 HeaderValue::from_str(&value).unwrap(),
             );
         }
